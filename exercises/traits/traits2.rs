53ccadd96ba6502fe24dba0e507408d407905742
mehtaishita <ishita.mehta33@outlook.com>
2022-11-14 16:28:19 -0600

    starting with traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -11,13 +11,18 @@
 // you can do this!
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-//TODO: Add your code here
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push(String::from("Bar")); // can't return this because change is happening in place
+        self
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -28,5 +33,15 @@ mod tests {
         let mut foo = vec![String::from("Foo")].append_bar();
         assert_eq!(foo.pop().unwrap(), String::from("Bar"));
         assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+    }
+
+    #[test]
+    fn is_vec_push_pop_eq_bar() {
+        let mut foo = vec![String::from("zoo")].append_bar();
+        // foo.push(String::from("foo")).append_bar(); - 
+            // doesn't work because 'push' changes in place and doesn't return self. 
+            // so 'append_bar doesn't exist on ()'
+        assert_eq!(foo.pop().unwrap(), String::from("Bar"));
+        assert_eq!(foo.pop().unwrap(), String::from("zoo"));
     }
 }
